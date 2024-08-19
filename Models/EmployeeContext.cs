f8f31cd2fcf99951c8fdfb2ba266aec06f49bfe9
ozanugurcolakk <ozanugurcolak@gmail.com>
2024-08-19 14:52:13 +0300

    Proje dosyası ekle.

---
+++ Models/EmployeeContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SampleCrud_Be.Models
+{
+    public class EmployeeContext : DbContext
+    {
+        public EmployeeContext(DbContextOptions<EmployeeContext> options) : base(options) 
+        {
+
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
